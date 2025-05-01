bdfb1be50620431144934d7a5bc7b921ecf06c69
krompac <tomijehrvat@gmail.com>
2025-05-01 20:18:05 +0200

    Change config for google drive api credentials.

--- src/server/routes/api/drive-upload.ts
+++ src/server/routes/api/drive-upload.ts
@@ -1,16 +1,31 @@
 // src/server/routes/api/drive-upload.ts
-import fs from 'fs';
 import { google } from 'googleapis';
 import { defineEventHandler, readMultipartFormData } from 'h3';
-import path from 'path';
 import { Readable } from 'stream';
 
-// Load service account credentials
-const CREDENTIALS_PATH = path.join(
-  process.cwd(),
-  'service-account-credentials.json'
-);
-const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, 'utf8'));
+const credentials = {
+  type: process.env['GOOGLE_SERVICE_ACCOUNT_TYPE'] || 'service_account',
+  project_id: process.env['GOOGLE_SERVICE_ACCOUNT_PROJECT_ID'],
+  private_key_id: process.env['GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID'],
+  private_key: process.env['GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY']?.replace(
+    /\\n/g,
+    '\n'
+  ),
+  client_email: process.env['GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL'],
+  client_id: process.env['GOOGLE_SERVICE_ACCOUNT_CLIENT_ID'],
+  auth_uri:
+    process.env['GOOGLE_SERVICE_ACCOUNT_AUTH_URI'] ||
+    'https://accounts.google.com/o/oauth2/auth',
+  token_uri:
+    process.env['GOOGLE_SERVICE_ACCOUNT_TOKEN_URI'] ||
+    'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url:
+    process.env['GOOGLE_SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL'] ||
+    'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env['GOOGLE_SERVICE_ACCOUNT_CLIENT_CERT_URL'],
+  universe_domain:
+    process.env['GOOGLE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN'] || 'googleapis.com',
+};
 
 // Create JWT client
 const auth = new google.auth.JWT(
