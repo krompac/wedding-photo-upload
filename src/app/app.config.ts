1ae6a14607e59a87d81cb9a6c3aa7c39b26df0ca
krompac <tomijehrvat@gmail.com>
2025-08-31 10:06:31 +0200

    Refactor config.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,9 +14,9 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideClientHydration } from '@angular/platform-browser';
 
 const options: FirebaseOptions = {
-  apiKey: process.env['FIREBASE_API_KEY'],
-  projectId: process.env['FIREBASE_PROJECT_ID'],
-  messagingSenderId: process.env['FIREBASE_SENDER_ID'],
+  apiKey: import.meta.env['VITE_FIREBASE_API_KEY'],
+  projectId: import.meta.env['VITE_FIREBASE_PROJECT_ID'],
+  messagingSenderId: import.meta.env['VITE_FIREBASE_SENDER_ID'],
 };
 
 export const appConfig: ApplicationConfig = {
